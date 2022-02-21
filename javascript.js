8c38a243f4a534cf2d974d476489df88f412478f
IsraelCavalcante58 <leopontorua@gmail.com>
2022-02-21 18:06:45 -0300

    Site Health Track

---
+++ javascript.js
@@ -0,0 +1,29 @@
+let time= 7000,
+   currentImageIndex = 0,
+   images = document.querySelectorAll("#slider div")
+   max = images.length;
+
+   function nextImage(){
+      images[currentImageIndex].classList.remove("selected") // Adicionar a classe na imagem atual, começando de 0
+
+      currentImageIndex++ // Mudando o valor do index, ou seja, apontando a próxima imagem sempre que passar pela função
+      if(currentImageIndex >= max){
+         currentImageIndex = 0
+         
+
+      }
+      images[currentImageIndex].classList.add("selected") // Adicionar a classe na imagem atual, começando de 0
+
+   }
+
+   function start(){
+      setInterval(() => {
+         // troca de imagem
+         nextImage()
+      }, time)
+      
+   }
+
+   window.addEventListener("load", start)
+
+
