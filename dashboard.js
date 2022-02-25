3294d3a0a166ff5a32270bd9cee3a1b37c9677b9
IsraelCavalcante58 <leopontorua@gmail.com>
2022-02-25 19:10:50 -0300

    Mudanças no Dashboard: Gauge e terminando a navegação do usuário

---
+++ dashboard.js
@@ -0,0 +1,68 @@
+// GRÁFICO DE CALORIAS/PASSOS
+var options = {
+
+
+
+    series: [{
+        name: 'Passos',
+        data: [3, 5, 4, 7, 9, 4, 7]
+    }, {
+        name: 'Calorias',
+        data: [0.1, 0.3, 0.2, 0.4, 0.1250, 0.2, 0.4]
+        }],
+    
+    chart: {
+        width: 681,
+        height: 363,
+        type: 'area'
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        show: true,
+        curve: ['smooth', 'smooth'],
+        lineCap: ['square', 'butt'],
+        colors: undefined,
+        width: 4,
+        dashArray: 0,
+    },
+
+    xaxis: {
+        type: 'datetime',
+        categories: ["2022-02-20", "2022-02-21", "2022-02-22", "2022-02-23", "2022-02-24", "2022-02-25", "2022-02-26"]
+    },
+    tooltip: {
+        x: {
+            format: 'dd/MM/yy'
+
+            
+            
+        },
+
+        
+
+    },
+    
+    
+
+    yaxis: {
+        show: false,
+
+    },
+
+    colors: ['#6D63FF', '#3d34c2']
+
+
+
+
+};
+
+
+
+
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
